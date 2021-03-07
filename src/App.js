673735ae35ab17a6d78cdb0b97d5653efc94c391
imran2197 <samimdimran786@gmail.com>
2021-03-07 11:24:47 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ app.post("/CardPayment", async(req, res) => {
     const body = req.body;
     const {cardNumber, expiryDate, cvCode, cardOwner} = body;
     if(isNullOrUndefined(cardNumber) || isNullOrUndefined(expiryDate) || isNullOrUndefined(cvCode) || isNullOrUndefined(cardOwner)){
-        res.status(500).json({message: "Please fill all the details"});
+        res.status(400).send({message: "Please fill all the details"});
     }
     else{
             const newCardPayment = new formModel(body);
